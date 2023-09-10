6537e9e3068c9fa0bf2f3424373b2f7968759c82
Anano24 <ananorobakidze24@gmail.com>
2023-09-10 16:02:59 +0400

    Add category and author models and make a new relationship with different tables. 1. Modify models.py, add category and author classes 2. Modify serializers.py, add new classes for category and author 3. Make new migrations 4. Modify views.py, add new classes for category and author 5. Modify urls.py, add new routers for category and author

--- tasks/serializers.py
+++ tasks/serializers.py
@@ -1,8 +1,19 @@
 from rest_framework import serializers
-from .models import Task
+from .models import Task, Author, Category
 
 
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = ('id', 'title', 'description', 'completed')
+        fields = '__all__'
+
+class AuthorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Author
+        fields = '__all__'
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, include
-from .views import TaskView
+from .views import TaskView, AuthorView, CategoryView
 from rest_framework import routers
 
 
 router = routers.DefaultRouter()
-router.register(r'', TaskView, 'task')
+router.register(r'tasks', TaskView, 'task')
+router.register(r'authors', AuthorView)
+router.register(r'categories', CategoryView)
+
 
 urlpatterns = [
     path('', include(router.urls)),
